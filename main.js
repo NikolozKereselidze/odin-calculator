0669ee3ee5fdbd306d47a9634e86bd69854ad1f8
NikolozKereselidze <kereselidze.nikoloz7@gmail.com>
2024-02-24 16:54:54 +0400

    implement selecting numbers and operator

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
 "use strict";
+
+let firstNumber = "";
+let secondNumber = "";
+let selectedOperation = null;
+const currentScreen = document.querySelector(".screen-current");
+
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.getElementById("equalsBtn");
+const clearButton = document.getElementById("clearBtn");
+const deleteButton = document.getElementById("deleteBtn");
+const pointButton = document.getElementById("pointBtn");
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (currentScreen.textContent === "0") {
+      currentScreen.textContent = button.textContent;
+    } else {
+      currentScreen.textContent += button.textContent;
+    }
+
+    if (selectedOperation === null) firstNumber += button.textContent;
+    else if (secondNumber === "") {
+      secondNumber = button.textContent;
+      currentScreen.textContent = secondNumber;
+    } else {
+      secondNumber += button.textContent;
+      console.log(secondNumber);
+    }
+  });
+});
+
+operatorButtons.forEach((operator) => {
+  operator.addEventListener("click", () => {
+    if (firstNumber) {
+      selectedOperation = operator.textContent;
+    } else return;
+  });
+});
+
+
